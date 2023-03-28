dbd010a5aa5cc1618c83fec48d7964cd40e55712
Wilbert <ramon.wilbert@accruent.com>
2023-03-28 13:23:12 -0300

    started Java

---
+++ JavaAlura/src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+
+/* 
+Tes comment
+
+API:
+https://imdb-api.com/en/API/Top250Movies/k_n3fyslp2
+
+https://imdb-api.com/api/#Top250Movies-header
+
+Imersão Java 2ª edição | Aula 01 | Desafio 01
+
+https://www.youtube.com/watch?v=v4Dpul7b5bU&ab_channel=Alura
+
+Main class:
+https://www.youtube.com/watch?v=D2NpRPs4CcE&ab_channel=Alura
+
+Read:
+https://www.alura.com.br/imersao-java/aula01-consumindo-api-com-java
+https://www.alura.com.br/artigos/desenvolvendo-aplicacoes-java-vs-code
+https://code.visualstudio.com/docs/java/java-tutorial#_coding-pack-for-java
+https://code.visualstudio.com/docs/languages/java#_install-visual-studio-code-for-java
+
+*/
