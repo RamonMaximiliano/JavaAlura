ad0b7eaa0cd95b2bbdf6412cd0031ee7babf9649
Wilbert <ramon.wilbert@accruent.com>
2023-04-03 11:10:00 -0300

    finished

--- JavaAlura/src/App.java
+++ JavaAlura/src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     /**
@@ -12,13 +14,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! This is a test");
 
+        //fazer uma conexão HTTP e buscar a lista de filmes
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();    
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
+
+        //extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body); 
+        System.out.println(listaDeFilmes.size());
+        System.out.println(listaDeFilmes);
     }
 }
 
